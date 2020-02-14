b2e5d220d26fbdd908874009667a8b558d06069f
walkskyer <walkskyer@qq.com>
2020-02-14 15:47:47 +0800

    shell build

---
+++ build.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+app_name=local_ws
+go build -o bin/${app_name} cmd/ws-local/local-ws.go
---
+++ go-get.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+go get golang.org/x/net/websocket
+go get github.com/codahale/chacha20
+go get golang.org/x/crypto/blowfish
+go get golang.org/x/crypto/cast5
+go get golang.org/x/crypto/salsa20/salsa
